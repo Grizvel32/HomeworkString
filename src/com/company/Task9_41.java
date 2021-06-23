dbe3a9ffafe9bd204d2963e99c55d8ac3d04d480
Grizvel32 <grizenkov1996@gmail.com>
2021-06-23 20:17:39 +0300

    first commit

---
+++ src/com/company/Task9_41.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Task9_41 {
+    public static void main(String[] args) {
+        String nameClub = "Manchester";
+
+        for (int i = 0; i < nameClub.length(); i++) {
+          System.out.println(nameClub.charAt(i));
+        }
+    }
+}
